6f01f4e4a5d705d50a142254fdba0058f89e3b92
Alen Joy <alenjoy@outlook.in>
2020-01-31 11:30:18 +0000

    inittial commit

--- ImageWeb/Pages/Index.cshtml.cs
+++ ImageWeb/Pages/Index.cshtml.cs
@@ -12,31 +12,54 @@ using Microsoft.Extensions.Logging;
 
 namespace ImageWeb.Pages
 {
-  public class IndexModel : PageModel
-  {
-    private HttpClient _httpClient;
-    private Options _options;
-    private readonly ILogger<IndexModel> _logger;
-
-    public List<string> ThumbnailImageList { get; private set; }
-    public List<string> FullImageList { get; private set; }
-    public IndexModel(HttpClient httpClient, Options options, ILogger<IndexModel> logger)
+    public class IndexModel : PageModel
     {
-      _httpClient = httpClient;
-      _options = options;
-      _logger = logger;
-    }
-    public async Task OnGetAsync()
-    {
-      return null;
-    }
+        private HttpClient _httpClient;
+        private Options _options;
+        private readonly ILogger<IndexModel> _logger;
 
-    [BindProperty]
-    public Microsoft.AspNetCore.Http.IFormFile Upload { get; set; }
+        public List<string> ThumbnailImageList { get; private set; }
+        public List<string> FullImageList { get; private set; }
+        public IndexModel(HttpClient httpClient, Options options, ILogger<IndexModel> logger)
+        {
+            _httpClient = httpClient;
+            _options = options;
+            _logger = logger;
+        }
+        public async Task OnGetAsync()
+        {
+            var imageUrl = _options.ApiUrl;
+            var thumbsUrl = Flurl.Url.Combine(imageUrl, "/thumbs/");
+            Task<string> getFullImages = _httpClient.GetStringAsync(imageUrl);
+            Task<string> getThumbNailImages = _httpClient.GetStringAsync(thumbsUrl);
+            await Task.WhenAll(getFullImages);
 
-    public async Task<IActionResult> OnPostAsync()
-    {
-      return null;
+            string fullImageJson = getFullImages.Result;
+            IEnumerable<string> fullImageList = JsonConvert.DeserializeObject<IEnumerable<string>>(fullImageJson);
+            FullImageList = fullImageList.ToList<string>();
+
+            string thumbImageJson = getThumbNailImages.Result;
+            IEnumerable<string> thumbImageList = JsonConvert.DeserializeObject<IEnumerable<string>>(thumbImageJson);
+            ThumbnailImageList = thumbImageList.ToList<string>();
+
+        }
+
+        [BindProperty]
+        public Microsoft.AspNetCore.Http.IFormFile Upload { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (Upload != null && Upload.Length > 0)
+            {
+                var imageUrl = _options.ApiUrl;
+                using (var image = new StreamContent(Upload.OpenReadStream()))
+                {
+                    image.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(Upload.ContentType);
+                    var response = await _httpClient.PostAsync(imageUrl, image);
+                }
+
+            }
+            return RedirectToPage("/Index");
+        }
     }
-  }
 }
